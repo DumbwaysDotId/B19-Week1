ed01f361dcabe3bdaedd9efad66fc8f831454985
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-11-24 10:18:43 +0700

    Embedding Expression with Class Component

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,23 +1,29 @@
 import React, {Component} from 'react';
 import "../App.css";
 
-// class Content extends Component{
-//     render(){
-//         return(
-//             <div className="App-content">
-//                 <p>This Is Content and Wellcome B19</p>
-//             </div>
-//         )
-//     }
-// }
+class Content extends Component{
 
+    getBatch(){
+        return 15+4;
+    }
 
-function Content(){
-    return(
-        <div className="App-content">
-            <p>This Is Content with Functional</p>
-        </div>
-    )
+    render(){
+        const companyName = "Dumbways.id";
+        return(
+            <div className="App-content">
+                <p>This Is Content and Wellcome {this.getBatch()} Bootcamp {companyName}</p>
+            </div>
+        )
+    }
 }
+
+
+// function Content(){
+//     return(
+//         <div className="App-content">
+//             <p>This Is Content with Functional</p>
+//         </div>
+//     )
+// }
 
 export default Content;
