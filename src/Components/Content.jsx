9588c0edf955718597da5f5f0a4795c13a7d1c5c
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-11-24 10:07:15 +0700

    Component with Class

---
+++ src/Components/Content.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import "../App.css";
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="App-content">
+                <p>This Is Content and Wellcome B19</p>
+            </div>
+        )
+    }
+}
+
+
+export default Header;
