b035b6fb2df72aad33b8c29653561c69fb75d5f7
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-11-25 12:27:13 +0700

    Routing

--- src/pages/arrayData.jsx
+++ src/pages/arrayData.jsx
@@ -1,10 +1,16 @@
-import '../App.css'
+import '../App.css';
+
+import {Link} from 'react-router-dom'
+
 function ArrayData(){
     
     const students = ["Vian", "Ilyas", "Seiga", "Satrian"];
     return(
         <div className="App-content">
             {students.map((student, index) => (<p key={index}>{student}</p>))}
+
+
+            <Link to="/objectData"><button>Object Data</button></Link>
         </div>
     )
 }
