27b8529a7a7f10f0857ee86e4eebee47c7adf91d
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-11-25 10:03:53 +0700

    Todo New

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,36 +1,9 @@
 import React, {useState} from 'react';
 import '../Todo.css';
 
-
-function Todo({ todo, index, completeTodo, removeTodo }){
-    return(
-        <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
-            {todo.text}
-            <div>
-                <button onClick={() => completeTodo(index)}>Complete</button>
-                <button onClick={() => removeTodo(index)}>X</button>
-            </div>
-        </div>
-    )
-}
-
-
-function TodoForm({addTodo}) {
-    const [value, setValue] = useState("");
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        if(!value) return;
-        addTodo(value)
-        setValue("")
-    }
-
-    return(
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-        </form>
-    )
-}
+//import Components
+import Todo from '../Components/ListTodo';
+import TodoForm from '../Components/TodoForm';
 
 
 function Master() {
